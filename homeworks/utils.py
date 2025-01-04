7bb504ff528c2f51fba355d92e9f31bb5adb4077
Robert Impey <robert-impey@users.noreply.github.com>
2025-01-04 00:29:23 +0000

    CH01: Ran notebook and fixed int errors

--- homeworks/utils.py
+++ homeworks/utils.py
@@ -23,7 +23,7 @@ class FixedWidthVariables(object):
         self.colspecs = variables[['start', 'end']] - index_base
 
         # convert colspecs to a list of pair of int
-        self.colspecs = self.colspecs.astype(np.int).values.tolist()
+        self.colspecs = self.colspecs.astype(np.int64).values.tolist()
         self.names = variables['name']
 
     def read_fixed_width(self, filename, **options):
